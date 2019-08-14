bf7c70f1e22ecaf8ecec8d84b797e6ea1ab1742e
natalie <nataliediazrodriguez@gmail.com>
2019-08-13 20:20:06 -0400

    add task

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#toggle_complete!" do
+    it "should switch complete to false if it was true" do
+      task = Task.create(complete: true)
+      task.toggle_complete!
+      expect(task.complete).to eq(false)
+    end
+
+    it "should switch complete to true if it was false" do
+      task = Task.create(complete: false)
+      task.toggle_complete!
+      expect(task.complete).to eq(true)
+    end
+  end
 end
